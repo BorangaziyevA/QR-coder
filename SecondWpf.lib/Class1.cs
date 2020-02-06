70131a1f6b29c9e05bcb9bd5f74c0459c775a43c
Алишер Борангазиев <@local.itstep>
2020-02-06 21:54:30 +0600

    Добавьте файлы проекта.

---
+++ SecondWpf.lib/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace SecondWpf.lib
+{
+    public class Qr
+    {
+        
+        static public void genQR(int size,string text)
+        {
+            var client = new RestClient("http://qrcoder.ru");
+
+            var request = new RestRequest($"code/?{text}&{size}&0");
+
+            var response =client.Execute(request) ;
+
+            var result = response.Content;
+            
+        }
+    }
+}
---
+++ SecondWpf/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SecondWpf.lib;
+
+namespace SecondWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //btnTh.Margin = new Thickness(100, 100, 100, 100);
+
+            //Border border = new Border();
+            //border.Margin = new Thickness(10);
+            //border.BorderBrush = new SolidColorBrush(Colors.Red);
+            //border.BorderThickness = new Thickness(20);
+            //border.Width = 200;
+            //border.Height = 200;
+
+            //MyWrap.Children.Add(border);
+
+
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            string text = tbxText.Text;
+            int size = 0;
+            foreach (RadioButton rb in spSize.Children)
+            {
+                if((bool)rb.IsChecked)
+                {
+                    size = Convert.ToInt32(rb.Content);
+                    break;
+                }
+            }
+            Qr.genQR(size,text);
+        }
+
+        //private void Button_click(object sender, RoutedEventArgs e)
+        //{
+        //    foreach (Border item in MyWrap.Children)
+        //    {
+        //        item.BorderBrush = new SolidColorBrush(Colors.Green);
+        //    }
+        //}
+    }
+}
